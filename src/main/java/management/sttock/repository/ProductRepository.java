54757091b9c93ff403326718d2405f8f085723f6
boyoung <28287@naver.com>
2023-05-28 15:57:21 +0900

    [feat] 상품 생성 개발 완료

--- src/main/java/management/sttock/repository/ProductRepository.java
+++ src/main/java/management/sttock/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package management.sttock.repository;
 
 import lombok.RequiredArgsConstructor;
+import management.sttock.domain.Member;
 import management.sttock.domain.Product;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -20,15 +24,41 @@ public class ProductRepository {
             em.merge(product); //dirty checking으로 수정 필요
         }
     }
-    public List<Product> findAll() {
-        return em.createQuery("select p from Product p", Product.class)
-                .getResultList();
+
+    public List<Product> findAllProducts(String userId) {
+        String jpql = "SELECT p FROM Product p " +
+                "JOIN p.member m " +
+                "WHERE m.userId = :userId";
+
+        TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+        query.setParameter("userId", userId);
+        try {
+            return query.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
-    public Product findOne(Long id) {
-        return em.find(Product.class, id);
+    public Product findOne(String userId, Long id) {
+        String jpql = "SELECT p FROM Product p " +
+                "JOIN p.member m " +
+                "WHERE m.userId = :userId " +
+                "AND p.id = :productId";
+
+        TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+        query.setParameter("userId", userId);
+        query.setParameter("productId", id);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
-    //자주 이용하는 상품 조회 추가 필요
+    public void deleteOne(Long id) {
+        Query query = em.createQuery("delete from Product p where p.id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
 
 }
