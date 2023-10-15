0cbcca4434ffdf688562f6d64f65d663a7435319
BOYOUNG-KANG <28287@naver.com>
2023-10-15 16:31:48 +0900

    feat: url 추가

--- src/main/java/management/sttock/common/define/ApiPath.java
+++ src/main/java/management/sttock/common/define/ApiPath.java
@@ -3,11 +3,10 @@ package management.sttock.common.define;
 public class ApiPath extends CorePath{
     /* defalut */
     private static final String V1 = "/api/v1";
-    private static  final String USER = "/user";
-
+    private static final String USER = "/user";
+    private static final String PRODUCT = "/product";
     /* user */
     public static final String V1_USER = V1 + USER;
     public static final String SIGN_UP = "/signup";
-
-
+    private static final String V1_PRODUCT = V1 + PRODUCT;
 }
