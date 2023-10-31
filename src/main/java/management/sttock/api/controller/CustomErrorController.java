391964a886ee30878a9786ad3cdd09c3f7fc40bd
BOYOUNG-KANG <28287@naver.com>
2023-10-31 17:51:09 +0900

    fix: /error 요청 시 반환 에러 변경 로직 수정

---
+++ src/main/java/management/sttock/api/controller/CustomErrorController.java
@@ -0,0 +1,17 @@
+package management.sttock.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import management.sttock.support.error.ApiException;
+import management.sttock.support.error.ErrorType;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+public class CustomErrorController implements ErrorController {
+    @GetMapping("/error")
+    public void throwError(ApiException apiException, Exception e){
+        log.info("/error 요청");
+        throw new ApiException(ErrorType.UNAUTHENTICATED_STATUS);
+    }
+}
